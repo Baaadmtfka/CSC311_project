536614635e32606929164f47416f08865ad3e856
Baaadmtfka <zixuan.zeng@mail.utoronto.ca>
2024-07-30 18:16:43 -0400

    Q2 IRT

--- item_response.py
+++ item_response.py
@@ -5,6 +5,7 @@ from utils import (
     load_train_sparse,
 )
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def sigmoid(x):
@@ -27,7 +28,21 @@ def neg_log_likelihood(data, theta, beta):
     # TODO:                                                             #
     # Implement the function as described in the docstring.             #
     #####################################################################
+
     log_lklihood = 0.0
+
+    for i in range(len(data["is_correct"])):
+        # user
+        u = data["user_id"][i]
+        theta_u = theta[u]
+        # question
+        q = data["question_id"][i]
+        beta_q = beta[q]
+        # c_ij
+        c = data["is_correct"][i]
+        # llk
+        log_lklihood += c * (theta_u - beta_q) - np.log(1 + np.exp(theta_u - beta_q))
+
     #####################################################################
     #                       END OF YOUR CODE                            #
     #####################################################################
@@ -55,7 +70,27 @@ def update_theta_beta(data, lr, theta, beta):
     # TODO:                                                             #
     # Implement the function as described in the docstring.             #
     #####################################################################
-    pass
+
+    # init derivatives
+    theta_deriv = np.zeros_like(theta)
+    beta_deriv = np.zeros_like(beta)
+
+    for i in range(len(data["is_correct"])):
+        # user
+        u = data["user_id"][i]
+        theta_u = theta[u]
+        # question
+        q = data["question_id"][i]
+        beta_q = beta[q]
+        # c_ij
+        c = data["is_correct"][i]
+        # update derivatives
+        theta_deriv[u] += c - sigmoid(theta_u - beta_q)
+        beta_deriv[q] += -c + sigmoid(theta_u - beta_q)
+
+    theta += lr * theta_deriv
+    beta += lr * beta_deriv
+
     #####################################################################
     #                       END OF YOUR CODE                            #
     #####################################################################
@@ -76,20 +111,28 @@ def irt(data, val_data, lr, iterations):
     :return: (theta, beta, val_acc_lst)
     """
     # TODO: Initialize theta and beta.
-    theta = None
-    beta = None
+
+    # init with constants assuming operating on current fixed dataset
+    theta = np.zeros(542)
+    beta = np.zeros(1774)
 
     val_acc_lst = []
+    train_llk_lst = []
+    val_llk_lst = []
 
     for i in range(iterations):
         neg_lld = neg_log_likelihood(data, theta=theta, beta=beta)
+        train_llk_lst.append(-neg_lld)
+        val_neg_lld = neg_log_likelihood(val_data, theta=theta, beta=beta)
+        val_llk_lst.append(-val_neg_lld)
+
         score = evaluate(data=val_data, theta=theta, beta=beta)
         val_acc_lst.append(score)
         print("NLLK: {} \t Score: {}".format(neg_lld, score))
         theta, beta = update_theta_beta(data, lr, theta, beta)
 
     # TODO: You may change the return values to achieve what you want.
-    return theta, beta, val_acc_lst
+    return theta, beta, val_acc_lst, train_llk_lst, val_llk_lst
 
 
 def evaluate(data, theta, beta):
@@ -122,7 +165,28 @@ def main():
     # Tune learning rate and number of iterations. With the implemented #
     # code, report the validation and test accuracy.                    #
     #####################################################################
-    pass
+
+    # hyper-parameters
+    lr = 0.008
+    iterations = 100
+
+    # irt
+    theta, beta, val_acc_lst, train_llk_lst, val_llk_lst = irt(train_data, val_data, lr, iterations)
+
+    # plot training curve
+    fig, (ax1, ax2) = plt.subplots(1, 2)
+    ax1.plot(list(range(iterations)), train_llk_lst, label="Training Log-Likelihood")
+    ax1.plot(list(range(iterations)), val_llk_lst, label="Validation Log-Likelihood")
+    ax1.set_xlabel("iterations")
+    ax1.set_ylabel("Log-Likelihood")
+    ax1.legend()
+
+    # final validation and test acc
+    val_score = evaluate(val_data, theta, beta)
+    test_score = evaluate(test_data, theta, beta)
+    print("Validation Accuracy: {}".format(val_score))
+    print("Test Accuracy: {}".format(test_score))
+
     #####################################################################
     #                       END OF YOUR CODE                            #
     #####################################################################
@@ -131,7 +195,27 @@ def main():
     # TODO:                                                             #
     # Implement part (d)                                                #
     #####################################################################
-    pass
+
+    # sample quesitons
+    j1 = 1
+    j2 = 1452
+    j3 = 666
+    #print(beta[j1], beta[j2], beta[j3])
+
+    # prob
+    p_j1 = sigmoid(theta - beta[j1])
+    p_j2 = sigmoid(theta - beta[j2])
+    p_j3 = sigmoid(theta - beta[j3])
+
+    ax2.plot(theta, p_j1, "o", label=f'Question {j1}')
+    ax2.plot(theta, p_j2, "o", label=f'Question {j2}')
+    ax2.plot(theta, p_j3, "o", label=f'Question {j3}')
+
+    ax2.set_xlabel("Theta (Student Ability)")
+    ax2.set_xlabel("Probability of Correct Response")
+    ax2.legend()
+    plt.show()
+
     #####################################################################
     #                       END OF YOUR CODE                            #
     #####################################################################
